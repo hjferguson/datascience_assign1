01386cb3a9653a4c7e0cea60f607c4ae9495b10d
Harlan Ferguson <harlanjazz@gmail.com>
2023-09-22 00:18:14 -0400

    finishes project requirements

--- main.py
+++ main.py
@@ -9,17 +9,24 @@ df = pd.read_csv('biostats.csv')
 age_column = df['Age'].to_numpy() #df object has built in method to covert to numpy array!
 
 #task 3
-# for age in age_column:
-#     print(age)
+for age in age_column:
+    print(age)
 
 #task 4
-# print("Average: " + str(age_column.mean()))
-# print("Standard Deviation: " + str(age_column.std()))
-# print("Min value: " + str(age_column.min()))
-# print("Max value: " + str(age_column.max()))
+print("Average: " + str(age_column.mean()))
+print("Standard Deviation: " + str(age_column.std()))
+print("Min value: " + str(age_column.min()))
+print("Max value: " + str(age_column.max()))
 
 #task5
-# print(np.percentile(age_column,30))
-# print(np.median(age_column))
-# print(np.percentile(age_column, 70))
+print(np.percentile(age_column,30))
+print(np.median(age_column))
+print(np.percentile(age_column, 70))
 
+#task6
+plt.hist(age_column, bins=10, color='purple', edgecolor='black')
+plt.title('Age Distribution - by Harlan')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+
+plt.show()
