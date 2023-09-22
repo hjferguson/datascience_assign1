10a15cb2fa7124fba72768f12a5fbbd8574528bf
Harlan Ferguson <harlanjazz@gmail.com>
2023-09-22 00:12:47 -0400

    adds percentile outputs

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-#convert file to dataframe
+#task 1
 df = pd.read_csv('biostats.csv')
 
+#task 2
 age_column = df['Age'].to_numpy() #df object has built in method to covert to numpy array!
 
-#Task 3
+#task 3
 # for age in age_column:
 #     print(age)
 
-print("Average: " + str(age_column.mean()))
-print("Standard Deviation: " + str(age_column.std()))
-print("Min value: " + str(age_column.min()))
-print("Max value: " + str(age_column.max()))
+#task 4
+# print("Average: " + str(age_column.mean()))
+# print("Standard Deviation: " + str(age_column.std()))
+# print("Min value: " + str(age_column.min()))
+# print("Max value: " + str(age_column.max()))
+
+#task5
+# print(np.percentile(age_column,30))
+# print(np.median(age_column))
+# print(np.percentile(age_column, 70))
+
