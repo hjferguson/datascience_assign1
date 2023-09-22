ba86ff10a5ab008d7106176c2d7bee9bb9a2060a
Harlan Ferguson <harlanjazz@gmail.com>
2023-09-22 00:07:23 -0400

    adds mean, std, min, max output

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ df = pd.read_csv('biostats.csv')
 age_column = df['Age'].to_numpy() #df object has built in method to covert to numpy array!
 
 #Task 3
-for age in age_column:
-    print(age)
+# for age in age_column:
+#     print(age)
+
+print("Average: " + str(age_column.mean()))
+print("Standard Deviation: " + str(age_column.std()))
+print("Min value: " + str(age_column.min()))
+print("Max value: " + str(age_column.max()))
